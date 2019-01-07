459be741f771e537a2f703bc1cb7d85bcb3f30d8
jack-scott <jack.scott6@hotmail.co.uk>
2019-01-07 16:00:14 +1100

    added file selection dialog

--- editor.py
+++ editor.py
@@ -21,11 +21,22 @@ class App(QWidget):
         self.resize(250, 150)
         self.move(300, 300)
         self.setWindowTitle('Editor')
-        self.show()
 
+        self.show()
 
     def buttonFunction(self):
         print("You pressed the button")
+        self.openFileNameDialog()
+
+    def openFileNameDialog(self):   #source https://pythonspot.com/pyqt5-file-dialog/
+        options = QFileDialog.Options()     #enumerated options in base 16. For displaying the file explorer
+        options |= QFileDialog.DontUseNativeDialog      #native dialog is used by default, this turns it off
+        title = "Choose an image"
+        startFolder = "" # "" chooses the last folder you were in
+        fileFilter = "Images (*.png *.xpm .jpg)"
+        fileName, _ = QFileDialog.getOpenFileName(self, title, startFolder, fileFilter, options=options)
+        if fileName:
+            print(fileName)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
