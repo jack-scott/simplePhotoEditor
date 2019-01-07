5d19bf52b40c411f7244a8d860a36fdd9674fde1
jack-scott <jack.scott6@hotmail.co.uk>
2019-01-07 13:32:00 +1100

    boiler plate code

---
+++ editor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Editor')
+    w.show()
+    
+    sys.exit(app.exec_())
