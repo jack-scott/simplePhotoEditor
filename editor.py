7b100bfcdea9d2585067d1a0c8a85fbae0c812cd
jack-scott <jack.scott6@hotmail.co.uk>
2019-01-07 15:23:56 +1100

    changed to python3, turned into class

--- editor.py
+++ editor.py
@@ -1,25 +1,36 @@
-#!/usr/bin/python2.7    
+#!/usr/bin/python3   
 
 #TODO make this platform independant
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QFileDialog
 
-def buttonFunction():
-    print "You pressed the button"
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+
+    def initUI(self):
+        btn = QPushButton('Import Image', self)
+        btn.clicked.connect(self.buttonFunction)
+        btn.setToolTip("Press this button to import an image")
+        btn.move(10, 10)
+
+        self.resize(250, 150)
+        self.move(300, 300)
+        self.setWindowTitle('Editor')
+        self.show()
+
+
+    def buttonFunction(self):
+        print("You pressed the button")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = QWidget()
-
-    btn = QPushButton('Import Image', w)
-    btn.clicked.connect(buttonFunction)
-    btn.setToolTip("Press this button to import an image")
-    btn.move(10, 10)
-
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Editor')
-    w.show()
-    
+    ex = App()
     sys.exit(app.exec_())
+
+    
+    
