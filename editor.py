ee5b1a41d32c1345de3da54c0728c9dfea48f41a
jack-scott <jack.scott6@hotmail.co.uk>
2019-01-07 14:02:19 +1100

    added button and function call

--- editor.py
+++ editor.py
@@ -1,11 +1,22 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python2.7    
+
+#TODO make this platform independant
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+
+def buttonFunction():
+    print "You pressed the button"
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = QWidget()
+
+    btn = QPushButton('Import Image', w)
+    btn.clicked.connect(buttonFunction)
+    btn.setToolTip("Press this button to import an image")
+    btn.move(10, 10)
+
     w.resize(250, 150)
     w.move(300, 300)
     w.setWindowTitle('Editor')
